3e9d2195e08a53905c2838479c845bf03d73a221
muzahid604 <604muzahid@gmail.com>
2022-03-11 12:06:29 +0600

    4th bug solved

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ const createPost = (post) => {
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
-  console.log(post)
+  // console.log(post)
   div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -152,7 +152,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
